cb9fc968ed0af03c94b4d8627bf563cb0da6ef3d
Daniel Hams <daniel.hams@gmail.com>
2020-01-14 18:10:23 +0000

    sgugshell: remove any use of LD_LIBRARYN32_PATH to see if we are now working.

--- sgugshell.sh
+++ sgugshell.sh
@@ -7,8 +7,10 @@ SGUG_LIB=$SGUG_ROOT/lib32
 
 export PATH=$SGUG_BIN:/usr/bin:/bin:/usr/sbin:/usr/bsd
 # Explicit library paths
-export LD_LIBRARYN32_PATH=$SGUG_LIB:/usr/lib32:/lib32:/usr/lib:/lib
-# Use this when sgug stuff has RPATH to reference the libs
+#export LD_LIBRARYN32_PATH=$SGUG_LIB:/usr/lib32:/lib32:/usr/lib:/lib
+# Without /usr/lib or /lib
+#export LD_LIBRARYN32_PATH=$SGUG_LIB:/usr/lib32:/lib32
+# Use this when sgug stuff has validated RPATH to reference the libs
 #export LD_LIBRARYN32_PATH=/usr/lib32:/lib32:/usr/lib:/lib
 export PKG_CONFIG_PATH=$SGUG_LIB/pkgconfig
 export MANPATH=$SGUG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/usr/share/catman/p_man:/usr/share/catman/u_man
