32de4ab9fd5099539b1eb7a9c40eae1e4178b36f
Daniel Hams <daniel.hams@gmail.com>
2020-01-16 22:21:43 +0000

    Workaround localisation issue with LC_ALL=C

--- sgugshell.sh
+++ sgugshell.sh
@@ -14,6 +14,8 @@ export PATH=$SGUG_BIN:/usr/bin:/bin:/usr/sbin:/usr/bsd
 #export LD_LIBRARYN32_PATH=/usr/lib32:/lib32:/usr/lib:/lib
 export PKG_CONFIG_PATH=$SGUG_LIB/pkgconfig
 export MANPATH=$SGUG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/usr/share/catman/p_man:/usr/share/catman/u_man
+# Work around localisation issue with libiconv
+export LC_ALL=C
 
 export PS1='[sgugshell \u@\h \W]\$ '
 exec bash --norc -i
