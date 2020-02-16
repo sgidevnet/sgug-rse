5a90c68b4c2fea32b8d259818d486110c37bc3e0
Erno Palonheimo <esp@iki.fi>
2020-02-16 08:37:48 +0200

    Make the sgug-specific startup file an actual rc file so that aliases can be set there too.

--- sgugshell.sh
+++ sgugshell.sh
@@ -39,6 +39,6 @@ export MANPATH=$SGUG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/u
 export LC_ALL=C
 
 export PS1='[sgugshell \u@\h \W]\$ '
-# If ~/.sgug_bashrc exists, source it.
-[ -e $HOME/.sgug_bashrc ] && source $HOME/.sgug_bashrc
+# If ~/.sgug_bashrc exists, use that as our init file.
+[ -e $HOME/.sgug_bashrc ] && exec bash --rcfile $HOME/.sgug_bashrc -i
 exec bash --norc -i
