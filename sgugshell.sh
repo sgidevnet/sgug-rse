08673bd2d9c261c6c6a878248e6bfcd8917bd4d5
Daniel Hams <daniel.hams@gmail.com>
2020-04-12 11:27:38 +0100

    sgugshell: put the setting of PKG_CONFIG_PATH in comments and explain the paths are searched anyway.

--- sgugshell.sh
+++ sgugshell.sh
@@ -30,14 +30,22 @@ SGUG_BIN=$SGUG_ROOT/bin
 SGUG_LIB=$SGUG_ROOT/lib32
 
 export PATH=$SGUG_BIN:/usr/bin/X11:/usr/bin:/bin:/usr/sbin:/usr/bsd
+
 # Explicit library paths
 export LD_LIBRARYN32_PATH=$SGUG_LIB:/usr/lib32:/lib32:/usr/lib:/lib
 # Without /usr/lib or /lib
 #export LD_LIBRARYN32_PATH=$SGUG_LIB:/usr/lib32:/lib32
+
 # Use this when sgug stuff has validated RPATH to reference the libs
 #export LD_LIBRARYN32_PATH=/usr/lib32:/lib32:/usr/lib:/lib
-export PKG_CONFIG_PATH=$SGUG_LIB/pkgconfig
+
+# NOTE: Setting pkgconfig path like below is unnecessary - the two
+# listed paths are the default - and are searched last resort
+# even when PKG_CONFIG_PATH is set to something.
+#export PKG_CONFIG_PATH=$SGUG_LIB/pkgconfig:$SGUG_ROOT/share/pkgconfig
+
 export MANPATH=$SGUG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/usr/share/catman/p_man:/usr/share/catman/u_man
+
 # Work around localisation issue with libiconv
 export LC_ALL=C
 
